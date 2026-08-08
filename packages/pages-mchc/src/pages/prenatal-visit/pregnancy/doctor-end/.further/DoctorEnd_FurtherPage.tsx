616a660801ecc50839718c82aac0f90a999a1ca7
XIONGZHIQIANG25\xzq <953761972@qq.com>
2026-08-08 10:39:39 +0800

    医生端电子签名、诊断、医嘱导入优化

--- packages/pages-mchc/src/pages/prenatal-visit/pregnancy/doctor-end/.further/DoctorEnd_FurtherPage.tsx
+++ packages/pages-mchc/src/pages/prenatal-visit/pregnancy/doctor-end/.further/DoctorEnd_FurtherPage.tsx
@@ -36,7 +36,7 @@ function DoctorEnd_Further(props: IDoctorEnd_FurtherProps) {
   } = props;
   const [diagnosesList, setDiagnosesList] = useState<IMchc_Doctor_Diagnoses[]>([])
 
-  const serialNo_q = getSearchParamsValue('serialNo')
+  const serialNo_q = getSearchParamsValue('serialNo')|| getSearchParamsValue('serialNO')
 
   const [visitsData, _setVisitsData] = useState<IMchc_Doctor_RvisitInfoOfOutpatient>()
 
@@ -155,16 +155,16 @@ function DoctorEnd_Further(props: IDoctorEnd_FurtherProps) {
     const serialNo = __serialNo ?? formData?.serialNo!
 
     if (mchcEnv.is('越秀妇幼')) {
-      const xxxa = diagnosesList.map(_ => ({
-        ..._,
-        serialNo,
-        outEmrId
+      const xxxa = diagnosesList.map((_) => ({
+          ..._,
+          serialNo,
+          outEmrId,
       }))
 
       SMchc_Doctor.new_diagnosis_list(xxxa).then(set_diagnosesList)
     }
 
-    var set_data = assign(newData, { outEmrId, serialNo })
+    var set_data = assign(newData, { outEmrId, serialNO: serialNo })
     const redata = await SMchc_Doctor.updateRvisitInfoOfOutpatient(set_data);
     after_save(redata);
 
@@ -239,8 +239,8 @@ function DoctorEnd_Further(props: IDoctorEnd_FurtherProps) {
         fuck
           ? null
           : <FurtherSidebar
-            serialNo={formData?.serialNo!}
-            setDiagnosesList={set_diagnosesList}
+            serialNo={formData?.serialNo || serialNo_q}
+            setDiagnosesList={setDiagnosesList}
             diagnosesList={diagnosesList}
             formData={formData}
 
@@ -277,6 +277,7 @@ function DoctorEnd_Further(props: IDoctorEnd_FurtherProps) {
           getLastRecord={getLastRecord}
           headerInfo={headerInfo}
           diagnosesList={diagnosesList}
+          setDiagnosesList={setDiagnosesList}
           isAllPregnancies={false}
           formChange={formChange}
         />
